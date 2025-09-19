60af02a80478ed83bdb6d538be7cf4b4b3e98505
Yustaf Pramsistya <37360793+taftphir@users.noreply.github.com>
2025-09-20 04:19:13 +0700

    Updated LoginPage.tsx

--- src/components/Auth/LoginPage.tsx
+++ src/components/Auth/LoginPage.tsx
@@ -37,7 +37,7 @@ export default function LoginPage() {
             <img 
               src={config.companyLogo} 
               alt="Company Logo" 
-              className="w-40 h-40 object-contain mx-auto mb-6" style="border:1px solid black;"
+              className="w-40 h-40 object-contain mx-auto mb-6" style={{border:1px solid black;}}
             />
           </div>
 
